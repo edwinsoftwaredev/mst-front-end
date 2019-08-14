c4cf6638db8f2733bae4bedc2e75624e7c39f6c6
Edwin <edwin_m_paz@hotmail.com>
2019-08-14 16:36:31 -0600

    Fetch current User

--- src/app/core/auth/account.service.ts
+++ src/app/core/auth/account.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpResponse} from '@angular/common/http';
 import {SERVER_API_URL} from '../../shared/app-constants';
 import {Observable} from 'rxjs';
+import {IUser} from '../../shared/model/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,18 @@ import {Observable} from 'rxjs';
 export class AccountService {
   constructor(private httpClient: HttpClient) { }
 
+  /**
+   * Method to register a User
+   * @param account to register -> login, email, password
+   */
   registerUser(account: any): Observable<HttpResponse<any>> {
     return this.httpClient.post<any>(SERVER_API_URL + 'api/account', account, {observe: 'response'});
+  }
+
+  /**
+   * Method to get the current logged user
+   */
+  fetch(): Observable<HttpResponse<IUser>> {
+    return this.httpClient.get<IUser>(SERVER_API_URL + 'api/account', {observe: 'response'});
   }
 }
--- src/app/shared/model/user.model.ts
+++ src/app/shared/model/user.model.ts
@@ -1,15 +1,15 @@
 export interface IUser {
-  id: number;
-  login: string;
-  email: string;
-  password: string;
+  id?: number;
+  login?: string;
+  email?: string;
+  password?: string;
 }
 
 export class UserModel implements IUser {
   constructor(
-    public id: number,
-    public login: string,
-    public email: string,
-    public password: string
+    public id?: number,
+    public login?: string,
+    public email?: string,
+    public password?: string
   ) {}
 }
