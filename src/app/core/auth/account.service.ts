72dfa0f7a5d9cd0d178ab5670ece6928b182e08f
Edwin <edwin_m_paz@hotmail.com>
2019-08-14 17:47:28 -0600

    Update account.service.ts
    Identify if user is authenticated

--- src/app/core/auth/account.service.ts
+++ src/app/core/auth/account.service.ts
@@ -8,6 +8,10 @@ import {IUser} from '../../shared/model/user.model';
   providedIn: 'root'
 })
 export class AccountService {
+
+  private user: IUser;
+  private userAuthenticated: boolean;
+
   constructor(private httpClient: HttpClient) { }
 
   /**
@@ -23,5 +27,29 @@ export class AccountService {
    */
   fetch(): Observable<HttpResponse<IUser>> {
     return this.httpClient.get<IUser>(SERVER_API_URL + 'api/account', {observe: 'response'});
+  }
+
+  identify(force: boolean): Promise<IUser> {
+    if (force) {
+      this.user = null;
+    }
+
+    if (this.user) {
+      return Promise.reject(this.user);
+    }
+
+    return this.fetch().toPromise().then((res: HttpResponse<IUser>) => {
+      const userAccount = res.body;
+
+      if (userAccount) {
+        this.user = userAccount;
+        this.userAuthenticated = true;
+      } else {
+        this.user = null;
+        this.userAuthenticated  = false;
+      }
+
+      return this.user;
+    });
   }
 }
