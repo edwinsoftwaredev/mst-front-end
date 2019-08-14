1636fae3cd21b633b3ab94f20135471db76dab18
Edwin <edwin_m_paz@hotmail.com>
2019-08-14 14:47:32 -0600

    Components generation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {SharedModule} from './shared/shared.module';
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { HomeComponent } from './home/home.component';
+import {HomeModule} from './home/home.module';
+import {AuthenticationModule} from './authentication/authentication.module';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     SharedModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule,
+    AuthenticationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
