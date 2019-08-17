681d9549825a9db171da19d7e09e1a6deb769b25
Edwin <edwin_m_paz@hotmail.com>
2019-08-17 14:07:56 -0600

    Update Login Component

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
+import {LoginService} from './login.service';
+import {CsrfTokenService} from '../../shared/services/csrf-token.service';
 
 @Component({
   selector: 'app-login',
@@ -10,10 +12,16 @@ export class LoginComponent implements OnInit {
 
   username: string;
   password: string;
+  tryLogin: boolean;
 
   constructor(
     private router: Router,
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute,
+    private loginService: LoginService,
+    private csrfTokenService: CsrfTokenService
+  ) {
+    this.csrfTokenService.isTokenPresent();
+  }
 
   ngOnInit() {
   }
@@ -21,6 +29,19 @@ export class LoginComponent implements OnInit {
   login(): void {
     // do login
       // check if user have Spotify tokens
+
+    this.tryLogin = true;
+
+    if (this.username.length !== 0 && this.password.length !== 0) {
+      const credentials = {
+        username: this.username.trim(),
+        password: this.password.trim()
+      };
+
+      this.loginService.authenticate(credentials, () => {
+        this.tryLogin = false;
+      });
+    }
 
     this.router.navigate(['../connect-spotify'], {relativeTo: this.route});
   }
